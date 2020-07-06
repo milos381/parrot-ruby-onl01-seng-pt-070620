5092307e6ea1d40632e5d0b7aa0121ba81140414
Milos Popovic <milos_popovic@outlook.com>
2020-07-06 18:45:44 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(word = "Squawk!")
-  puts"#{word}"
-  word
-  puts "Pretty bird!"
   "Pretty bird!"
+  puts"#{word}"
+  puts "Pretty bird!"
+
 end
